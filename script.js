dfa4e169f5bd7a77ad204aea52e443f6a78a6d59
Mohankumar <91587795+Agathokakological7@users.noreply.github.com>
2023-08-07 17:09:45 +0530

    Update script.js
    added extra fonts

--- script.js
+++ script.js
@@ -9,7 +9,108 @@ let spacingButtons = document.querySelectorAll(".spacing");
 let formatButtons = document.querySelectorAll(".format");
 let scriptButtons = document.querySelectorAll(".script");
 
-let fontList = [
+const fontList = [
+    "Abel",
+    "Abril+Fatface",
+    "Acme",
+    "Alegreya",
+    "Alegreya+Sans",
+    "Anton",
+    "Archivo",
+    "Archivo+Black",
+    "Archivo+Narrow",
+    "Arimo",
+    "Arvo",
+    "Asap",
+    "Asap+Condensed",
+    "Bitter",
+    "Bowlby+One+SC",
+    "Bree+Serif",
+    "Cabin",
+    "Cairo",
+    "Catamaran",
+    "Crete+Round",
+    "Crimson+Text",
+    "Cuprum",
+    "Dancing+Script",
+    "Dosis",
+    "Droid+Sans",
+    "Droid+Serif",
+    "EB+Garamond",
+    "Exo",
+    "Exo+2",
+    "Faustina",
+    "Fira+Sans",
+    "Fjalla+One",
+    "Francois+One",
+    "Gloria+Hallelujah",
+    "Hind",
+    "Inconsolata",
+    "Indie+Flower",
+    "Josefin+Sans",
+    "Julee",
+    "Karla",
+    "Lato",
+    "Libre+Baskerville",
+    "Libre+Franklin",
+    "Lobster",
+    "Lora",
+    "Mada",
+    "Manuale",
+    "Maven+Pro",
+    "Merriweather",
+    "Merriweather+Sans",
+    "Montserrat",
+    "Montserrat+Subrayada",
+    "Mukta+Vaani",
+    "Muli",
+    "Noto+Sans",
+    "Noto+Serif",
+    "Nunito",
+    "Open+Sans",
+    "Open+Sans+Condensed:300",
+    "Oswald",
+    "Oxygen",
+    "PT+Sans",
+    "PT+Sans+Caption",
+    "PT+Sans+Narrow",
+    "PT+Serif",
+    "Pacifico",
+    "Passion+One",
+    "Pathway+Gothic+One",
+    "Play",
+    "Playfair+Display",
+    "Poppins",
+    "Questrial",
+    "Quicksand",
+    "Raleway",
+    "Roboto",
+    "Roboto+Condensed",
+    "Roboto+Mono",
+    "Roboto+Slab",
+    "Ropa+Sans",
+    "Rubik",
+    "Saira",
+    "Saira+Condensed",
+    "Saira+Extra+Condensed",
+    "Saira+Semi+Condensed",
+    "Sedgwick+Ave",
+    "Sedgwick+Ave+Display",
+    "Shadows+Into+Light",
+    "Signika",
+    "Slabo+27px",
+    "Source+Code+Pro",
+    "Source+Sans+Pro",
+    "Spectral",
+    "Titillium+Web",
+    "Ubuntu",
+    "Ubuntu+Condensed",
+    "Varela+Round",
+    "Vollkorn",
+    "Work+Sans",
+    "Yanone+Kaffeesatz",
+    "Zilla+Slab",
+    "Zilla+Slab+Highlight",
     "Arial",
     "Verdana",
     "Times New Roman",
@@ -17,7 +118,13 @@ let fontList = [
     "Georgia",
     "Courier New",
     "Cursive",
-];
+    "Roboto",      
+    "Open Sans",   
+    "Montserrat" , 
+  ];
+  
+
+  
 
 const intializer = () => {
     highlighter(alignButtons, true);
@@ -31,6 +138,7 @@ const intializer = () => {
         option.innerHTML = value;
         fontName.appendChild(option);
     });
+    
 
     for (let i = 1; i <= 7; i++) {
         let option = document.createElement("option");
@@ -94,4 +202,4 @@ const highlighterRemover = (className) => {
 };
 
 window.onload = intializer();
-Footer
+
